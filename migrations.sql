b18d5462fc87da0a470ee0b84923dc9053df5763
Valentim-Gab <gabrielvalentimxavier@gmail.com>
2023-10-26 12:13:15 -0300

    Adicionando novas tabelas

--- migrations.sql
+++ migrations.sql
@@ -1,7 +1,7 @@
-CREATE TABLE sex (
-	id_sex SERIAL PRIMARY KEY,
-	descr_sex VARCHAR(20) NOT NULL,
-	abbr_sex CHAR NOT NULL
+CREATE TABLE gender (
+	id_gender SERIAL PRIMARY KEY,
+	name VARCHAR(20) NOT NULL,
+	abbreviation CHAR NOT NULL
 );
 
 CREATE TABLE users (
@@ -17,8 +17,8 @@ CREATE TABLE users (
 	phone_number VARCHAR(25),
 	role varchar(10)[],
 	profile_image TEXT,
-	id_sex INT NOT NULL,
-	FOREIGN KEY (id_sex) REFERENCES sex (id_sex)
+	id_gender INT NOT NULL,
+	FOREIGN KEY (id_gender) REFERENCES gender (id_gender)
 );
 
 CREATE TABLE user_address (
@@ -38,10 +38,97 @@ CREATE TABLE user_address (
 	FOREIGN KEY (id_user) REFERENCES users (id_user)
 );
 
-INSERT INTO sex (descr_sex, abbr_sex) VALUES
-('MASCULINO', 'M'),
-('FEMININO', 'F'),
-('OUTRO', 'O');
+/* Exemplo: Nike; Adidas; Mizuno. */
+CREATE TABLE product_brand (
+  id_product_brand SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL
+);
+
+/* Exemplo: XPP, PP; P; M; G; GG; XGG ot */
+CREATE TABLE product_size (
+  id_product_size SERIAL PRIMARY KEY,
+  name VARCHAR(5)
+);
+
+/* Exemplo: Vermelho; Azul; Preto */
+CREATE TABLE product_color (
+  id_product_color SERIAL PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+  hexadecimal_code VARCHAR(10)
+);
+
+/* Exemplo: Másculino; Feminino; Unissex; Infantil; Esporte/Academia. */
+CREATE TABLE product_main_category (
+  id_product_main_category SERIAL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL
+);
+
+/* Exemplo: Calçados; Roupas; Acessórios; Esporte. */
+CREATE TABLE product_intermediate_category (
+  id_product_intermediate_category SERIAL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL
+);
+
+/* Exemplo: Botas; Tênis; Camisas; Vestidos. */
+CREATE TABLE product_subcategory (
+  id_product_subcategory SERIAL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL
+);
+
+/* Exemplo: Vestido - Roupa - Feminina. */
+CREATE TABLE categories_relation (
+  id_categories_relation SERIAL PRIMARY KEY,
+  description VARCHAR(100) NOT NULL,
+  id_main_category INTEGER NOT NULL,
+  id_intermediate_category INTEGER NOT NULL,
+  id_subcategory INTEGER NOT NULL,
+  FOREIGN KEY (id_product_main_category) REFERENCES product_main_category (id_product_main_category),
+  FOREIGN KEY (id_product_intermediate_category) REFERENCES product_intermediate_category (id_product_intermediate_category),
+  FOREIGN KEY (id_subcategory) REFERENCES subcategory (id_subcategory)
+);
+
+/* Exemplo: Vestido Curto */
+CREATE TABLE product (
+  id_product UUID UNIQUE PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  description TEXT NOT NULL,
+  units INTEGER NOT NULL DEFAULT 1,
+  price MONEY NOT NULL,
+  main_image TEXT,
+  images TEXT[],
+  numeric_size INTEGER,
+  id_product_brand INTEGER,
+  id_product_size INTEGER NOT NULL,
+  id_product_color INTEGER,
+  FOREIGN KEY (id_product_brand) REFERENCES product_brand (id_product_brand),
+  FOREIGN KEY (id_product_size) REFERENCES product_size (id_product_size),
+  FOREIGN KEY (id_product_color) REFERENCES product_color (id_product_color)
+);
+
+/* Exemplo: Vestido Curto, Vestido - Roupa - Feminina. */
+CREATE TABLE product_category_relation (
+  id_product_category_relation SERIAL PRIMARY KEY,
+  id_categories_relation INTEGER NOT NULL,
+  id_product UUID NOT NULL,
+  FOREIGN KEY (id_categories_relation) REFERENCES categories_relation (id_categories_relation),
+  FOREIGN KEY (id_product) REFERENCES product (id_product)
+);
+
+CREATE TABLE product_sale (
+  id_product_sale SERIAL PRIMARY KEY,
+  units INTEGER NOT NULL DEFAULT 1,
+  total_value MONEY NOT NULL,
+  id_product UUID NOT NULL,
+  id_user UUID NOT NULL,
+  FOREIGN KEY (id_product) REFERENCES product (id_product),
+  FOREIGN KEY (id_user) REFERENCES users (id_user)
+);
+
+
+/* CREATE INDEX idx_name_product ON product(name_product);  */
+
+INSERT INTO gender (name, abbreviation) VALUES
+('MASCULINO', 'M'), ('FEMININO', 'F'), ('OUTRO', 'O');
 
 INSERT INTO users (id_user, name, email, password, id_sex, role) VALUES (
 	'dc7fb99a-2f8a-46bb-a915-2a5fa911a155',
@@ -49,3 +136,24 @@ INSERT INTO users (id_user, name, email, password, id_sex, role) VALUES (
 	'$2b$10$LKA.RVeztsScuvW0PSfrUOivtcl/UpSZ48RlrnOHAy2IzM9mgutx2',
 	3, ARRAY['admin']
 );
+
+INSERT INTO product_brand (name) VALUES
+('Nike'), ('Adidas');
+
+INSERT INTO product_size (name) VALUES
+('XP'), ('PP'), ('P'), ('M'), ('G'), ('GG'), ('XG');
+
+INSERT INTO product_color (name, hexadecimal_code) VALUES
+('Vermelho', '#FF0000'),
+('Verde', '#00FF00'),
+('Azul', '#0000FF'),
+('Amarelo', '#FFFF00'),
+('Rosa', '#FFC0CB'),
+('Laranja', '#FFA500'),
+('Roxo', '#800080'),
+('Ciano', '#00FFFF'),
+('Marrom', '#A52A2A'),
+('Preto', '#000000'),
+('Branco', '#FFFFFF'),
+('Cinza', '#808080');
+
